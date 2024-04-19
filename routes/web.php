475ef752a538044517c4cdbc29f6daac26631afe
jakhin03 <caokhanh03.ck@gmail.com>
2024-04-19 18:12:01 +0000

    Setup Laravel Framework

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('landingPage');
+});
+
+Route::get('chat', function(){
+    return "view('chat')";
+});
+
+Route::get('/test', function(){
+    return view('landingPage');
+});
